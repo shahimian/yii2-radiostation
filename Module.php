0d03bc30aed47b59ceb828545a24e648b51f0cf9
Shahimian <shahimian@gmail.com>
2020-05-06 19:44:22 +0430

    initial radio station project

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace shahimian\radiostation;
+
+/**
+ * radiostation module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'shahimian\radiostation\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # yii2-radiostation
+
+To access the module, you need to add this to your application configuration:
+
+``
+<?php
+    ......
+    'modules' => [
+        'radiostation' => [
+            'class' => 'shahimian\radiostation\Module',
+        ],
+    ],
+    ......
+``
